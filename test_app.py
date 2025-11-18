04e78e0f90736ea3534e9e043922a452ee4f4a65
Gabe-Manaog <alonzogabriel.manaog@ontariotechu.net>
2025-11-17 23:08:51 -0500

    Added New POST route and Test for Post Route

--- test_app.py
+++ test_app.py
@@ -1,6 +1,15 @@
 # test_app.py
 import app
+from flask import json
+import app
 
+def test_echo():
+    client = app.app.test_client()
+    payload = {"msg": "ping"}
+    response = client.post('/echo', json=payload)
+    assert response.status_code == 201
+    assert response.get_json() == payload
+    
 def test_hello():
     client = app.app.test_client()
     response = client.get('/hello')
